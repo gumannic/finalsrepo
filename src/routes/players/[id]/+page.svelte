8055f62f90a5f301d0d05af46854577b61b7d875
Nick <nickgumann@bluewin.ch>
2025-05-30 17:49:21 +0200

    Finish

--- src/routes/players/[id]/+page.svelte
+++ src/routes/players/[id]/+page.svelte
@@ -10,5 +10,20 @@
     ← Back
   </button>
 
-  <h1>{player.first_name} {player.last_name}</h1>
+  <div class="card shadow-lg p-4">
+    <div class="row g-4 align-items-center">
+      <div class="col-md-4 text-center">
+        <img src={`/${player.pic}`} class="img-fluid rounded-3" alt={player.last_name}>
+      </div>
+      <div class="col-md-8">
+        <h2 class="mb-3">{player.first_name} {player.last_name}</h2>
+        <ul class="list-group list-group-flush fs-5">
+          <li class="list-group-item"><strong>Alter:</strong> {player.age}</li>
+          <li class="list-group-item"><strong>Club:</strong> {player.team}</li>
+          <li class="list-group-item"><strong>Nationalität:</strong> {player.nationality}</li>
+          <li class="list-group-item"><strong>Ablösesumme:</strong> {player.releaseClause} Mio. €</li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </div>
